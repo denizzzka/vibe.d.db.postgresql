53317ec13762a476524699ff78ba93db04c77f90
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-13 04:14:49 +0700

    logger setup

--- source/app.d
+++ source/app.d
@@ -1,6 +1,30 @@
-import vibe.core.args;
+import std.getopt;
+import std.experimental.logger;
+
+shared static this()
+{
+    sharedLog.fatalHandler = null;
+}
+
+string configFileName = "/wrong/path/to/file.json";
+bool debugEnabled = false;
 
 int main(string[] args)
 {
+    try
+    {
+        auto helpInformation = getopt(
+                args,
+                "debug", &debugEnabled,
+                "config", &configFileName
+            );
+    }
+    catch(Exception e)
+    {
+        fatal(e.msg);
+    }
+
+    if(!debugEnabled) sharedLog.logLevel = LogLevel.warning;
+
     return 0;
 }
