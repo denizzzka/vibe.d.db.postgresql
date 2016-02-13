2c73eb26b837ef92e566ac06f75d8e0d5a424ecd
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-02-14 06:54:36 +0700

    connectionFactory() as pool method

--- source/pool.d
+++ source/pool.d
@@ -5,16 +5,33 @@ import vibe = vibe.core.connectionpool;
 
 class ConnectionPool : vibe.ConnectionPool!(dpq2.Connection)
 {
+    private const string connString;
+
     this(string connString, uint connNum)
     {
-        dpq2.Connection connectionFactory()
-        {
-            auto c = new dpq2.Connection;
-            c.connString = connString;
-
-            return c;
-        }
+        this.connString = connString;
 
         super(&connectionFactory, connNum);
     }
+
+    private dpq2.Connection connectionFactory()
+    {
+        auto c = new dpq2.Connection;
+        c.connString = connString;
+        c.connectStart;
+
+        return c;
+    }
+}
+
+unittest
+{
+    auto pool = new ConnectionPool("wrong connection string", 3);
+
+    try
+        pool.lockConnection;
+    catch(dpq2.ConnectionException e)
+        return;
+
+    assert(false);
 }
