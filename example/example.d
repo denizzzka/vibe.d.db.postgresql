15728ea5eb649fb6bc26300934c27101c94bbfd2
Denis Feklushkin <denis.feklushkin@gmail.com>
2016-03-07 05:22:59 +0700

    example added

---
+++ example/example.d
@@ -0,0 +1,32 @@
+module vibe.db.postgresql.example;
+
+import vibe.d;
+import vibe.db.postgresql;
+
+PostgresClient client;
+
+void test()
+{
+    logInfo("asdasd");
+    auto conn = client.lockConnection();
+    immutable result = conn.execStatement(
+        "SELECT 123 as first_num, 567 as second_num, 'abc'::text as third_text "~
+        "UNION ALL "~
+        "SELECT 890, 233, 'fgh'::text as third_text",
+        ValueFormat.BINARY
+    );
+
+    assert(result[0]["second_num"].as!PGinteger == 567);
+    assert(result[1]["third_text"].as!PGtext == "fgh");
+
+    foreach (val; rangify(result[0]))
+        logInfo("Found entry: %s", val.toBson.toJson);
+}
+
+shared static this()
+{
+    // params: conninfo string, number of simultaneous connections
+    client = connectPostgresDB("dbname=postgres user=postgres", 4);
+
+    test();
+}
