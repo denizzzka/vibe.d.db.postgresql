1381e53e4ea6b96d8bc55187e7312d160371ebbd
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-10-06 13:25:58 +0700

    GC.collect() added to example

--- example/example.d
+++ example/example.d
@@ -7,20 +7,36 @@ PostgresClient client;
 
 void test()
 {
-    auto conn = client.lockConnection();
-    immutable result = conn.execStatement(
-        "SELECT 123 as first_num, 567 as second_num, 'abc'::text as third_text "~
-        "UNION ALL "~
-        "SELECT 890, 233, 'fgh'::text as third_text",
-        ValueFormat.BINARY
-    );
-    delete conn;
+    try
+    {
+        auto conn = client.lockConnection();
 
-    assert(result[0]["second_num"].as!PGinteger == 567);
-    assert(result[1]["third_text"].as!PGtext == "fgh");
+        immutable result = conn.execStatement(
+            "SELECT 123 as first_num, 567 as second_num, 'abc'::text as third_text "~
+            "UNION ALL "~
+            "SELECT 890, 233, 'fgh'::text as third_text",
+            ValueFormat.BINARY
+        );
 
-    foreach (val; rangify(result[0]))
-        logInfo("Found entry: %s", val.as!Bson.toJson);
+        delete conn;
+
+        assert(result[0]["second_num"].as!PGinteger == 567);
+        assert(result[1]["third_text"].as!PGtext == "fgh");
+
+        foreach (val; rangify(result[0]))
+            logInfo("Found entry: %s", val.as!Bson.toJson);
+
+        import core.memory;
+        GC.collect();
+    }
+    catch(ConnectionException e)
+    {
+        logWarn(e.msg);
+    }
+    catch(Exception e)
+    {
+        logWarn(e.msg);
+    }
 }
 
 static this()
