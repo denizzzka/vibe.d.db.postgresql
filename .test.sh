ca05b665b03629391cb3bdced4d63c57fe1e5390
Denis Feklushkin <denis.feklushkin@gmail.com>
2018-02-08 15:25:44 +0700

    adds CI for vibe-core

--- .test.sh
+++ .test.sh
@@ -3,6 +3,7 @@ set -ev
 
 dub test
 dub run --config=integration_test -- --conninfo="${1}" --debug=true
+dub run --config=integration_test_vibe_core -- --conninfo="${1}" --debug=true
 dub build :example
 
 #if [[ ${DC} -eq "dmd" ]]; then dub run dscanner -- -s; fi #disabled due to stall
