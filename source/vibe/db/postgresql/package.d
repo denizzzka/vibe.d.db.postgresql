9db5327b2d49cc631282546a620c33b7410f7d35
Denis Feklushkin <denis.feklushkin@gmail.com>
2019-01-29 09:38:56 +0700

    comment

--- source/vibe/db/postgresql/package.d
+++ source/vibe/db/postgresql/package.d
@@ -270,8 +270,7 @@ class Dpq2Connection : dpq2.Connection
 
     /// Row-by-row version of execStatement
     ///
-    /// Delegate will be called for each received Answer piece.
-    /// These answers will be have only one row.
+    /// Delegate will be called for each received row.
     ///
     /// More info: https://www.postgresql.org/docs/current/libpq-single-row-mode.html
     void execStatementRbR(in ref QueryParams params, void delegate(immutable(Row)) answerRowProcessDg)
