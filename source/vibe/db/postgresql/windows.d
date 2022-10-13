f00ca0b14e8db943f8522211636428da6fed0be1
Denis Feklushkin <feklushkin.denis@gmail.com>
2022-10-13 14:10:55 +0300

    Moved from external windows-headers package to internal declarations
    https://github.com/etcimon/windows-headers/issues/12

--- source/vibe/db/postgresql/windows.d
+++ source/vibe/db/postgresql/windows.d
@@ -2,11 +2,11 @@ module vibe.db.postgresql.windows;
 
 version(Windows):
 
-import core.time: Duration, dur;
+import core.time: Duration;
+import core.sys.windows.winsock2;
 import std.exception: enforce;
 import std.conv: to;
 import std.socket: SOCKET;
-import windows.winsock2;
 import dpq2.connection: ConnectionException;
 
 package SocketEvent createFileDescriptorEvent(SOCKET _socket)
@@ -30,14 +30,69 @@ package SocketEvent createFileDescriptorEvent(SOCKET _socket)
 struct SocketEvent
 {
     private WSAEVENT event;
+    //TODO: deregister events if struct destroyed?
 
     void wait()
     {
-        assert(false);
+        if(!waitImpl(WSA_INFINITE))
+            throw new ConnectionException("WSA_WAIT_TIMEOUT during WSA_INFINITE awaiting");
     }
 
     bool wait(Duration timeout)
     {
-        assert(false);
+        return waitImpl(timeout.total!"msecs".to!int);
     }
+
+    private bool waitImpl(DWORD timeout)
+    {
+        auto idx = WSAWaitForMultipleEvents(1, &event, false, timeout, false);
+
+        if(idx == WSA_WAIT_FAILED)
+            throw new ConnectionException("WSAWaitForMultipleEvents error, code "~WSAGetLastError().to!string);
+
+        if(WSAResetEvent(event) == false)
+            throw new ConnectionException("WSAResetEvent error, code "~WSAGetLastError().to!string);
+
+        if(idx == WSA_WAIT_TIMEOUT)
+            return false;
+
+        return true;
+    }
+}
+
+private:
+
+alias WSAEVENT = void*;
+
+// Not documented on MSDN
+enum {
+	FD_READ_BIT,
+	FD_WRITE_BIT,
+	FD_OOB_BIT,
+	FD_ACCEPT_BIT,
+	FD_CONNECT_BIT,
+	FD_CLOSE_BIT,
+	FD_QOS_BIT,
+	FD_GROUP_QOS_BIT,
+	FD_ROUTING_INTERFACE_CHANGE_BIT,
+	FD_ADDRESS_LIST_CHANGE_BIT,
+	FD_MAX_EVENTS // = 10
+}
+
+import core.sys.windows.winbase;
+
+const WSA_INFINITE = INFINITE;
+const WSA_INVALID_EVENT = WSAEVENT.init;
+const WSA_WAIT_FAILED = cast(DWORD)-1;
+const WSA_WAIT_TIMEOUT = WAIT_TIMEOUT;
+const int FD_READ = 1 << FD_READ_BIT;
+
+extern(Windows) nothrow @nogc
+{
+    import core.sys.windows.windef;
+
+    WSAEVENT WSACreateEvent();
+    int WSAEventSelect(SOCKET, WSAEVENT, int);
+    BOOL WSAResetEvent(WSAEVENT);
+    DWORD WSAWaitForMultipleEvents(DWORD, const(WSAEVENT)*, BOOL, DWORD, BOOL);
 }
